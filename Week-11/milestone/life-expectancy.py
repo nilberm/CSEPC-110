cac5e95c823442110954b52c11132cd9b86e12be
nilberm <nilber16@outlook.com>
2022-07-09 10:05:39 -0300

    week 11/12 activity

--- Week-11/milestone/life-expectancy.py
+++ Week-11/milestone/life-expectancy.py
@@ -1,86 +1,91 @@
-country_name = []
-country_initials = []
-years = []
-life_expectancy = []
-
-with open("life-expectancy.csv") as life_expectancy_file:
-  for line in life_expectancy_file:
-
-    list_info = line.split(",")
-    country_name.append(list_info[0])
-    country_initials.append(list_info[1])
-    years.append(list_info[2])
-    life_expectancy.append(list_info[3].strip("\n"))
-
-
-index_highest_expectancy = 0
-index_lowest_expectancy = 0
-
-value_life = -1
-value_life2 = 9999999
-
-for i, value in enumerate(life_expectancy):
-  value = float(value)
-  if value > value_life:
-    value_life = value
-    index_highest_expectancy = i
-  if value < value_life2:
-    value_life2 = value
-    index_lowest_expectancy = i
 
 
 
-# user_year = input("\nEnter the year of interest: ")
+lowest = 1000
+lowestYear = 1000
+lowestCountry = ""
 
-print(f"\nThe overall max life expectancy is: {life_expectancy[index_highest_expectancy]} from {country_name[index_highest_expectancy]} in {years[index_highest_expectancy]}")
-print(f"The overall min life expectancy is: {life_expectancy[index_lowest_expectancy]} from {country_name[index_lowest_expectancy]} in {years[index_lowest_expectancy]}")
+higher = -1
+higherYear = 1
+higherCountry = ""
+
+lowestChoice = 1000
+lowestCountryChoice = ""
+
+higherChoice = -1
+higherCountryChoice = ""
 
 
-# index_user_year = years.index(user_year)
+lowestChoiceCountry = 1000
+lowestYearChoiceCountry = 1000
 
-# count_years = 0
-# count_years_amount = 0
+higherChoiceCountry = -1
+higherYearChoiceCountry = 1
 
-# index_highest_expectancy_year = 0
-# index_lowest_expectancy_year = 0
 
-# value_life_year = -1
-# value_life2_year = 9999999
 
-# country_name_user = []
-# country_initials_user = []
-# years_user = []
-# life_expectancy_user = []
+choiceYear = int(input("Enter the year of interest: "))
+choiceCountry = input("Enter the country of interest: ")
+average = 0
+numChoiceYear = 0
 
-# for i, value in enumerate(years):
-#   if value == user_year:
-#     count_years += 1
-#     count_years_amount += float(life_expectancy[i])
 
-#     country_name_user.append(country_name[i])
-#     country_initials_user.append(country_initials[i])
-#     years_user.append(years[i])
-#     life_expectancy_user.append(life_expectancy[i])
+with open("Week-11\milestone\life-expectancy.csv") as lifeFile:
+  for line in lifeFile:
+    cleanLine = line.strip()
+    words = cleanLine.split(",")
 
+    country = words[0]
+    code = words[1]
+    year = int(words[2])
+    lifeExp = float(words[3])
+
+    if higher < lifeExp:
+      higher = lifeExp
+      higherYear = year
+      higherCountry = country
     
-#     value_float = float(value)
-#     if value_float > value_life_year:
-#       value_life_year = value_float
-#       index_highest_expectancy_year = i
-#     if value_float < value_life2_year:
-#       value_life2_year = value_float
-#       index_lowest_expectancy_year = i
+    if lowest > lifeExp:
+      lowest = lifeExp
+      lowestYear = year
+      lowestCountry = country
+
+    # Year Choice Code
+    if choiceYear == year:
+      average += lifeExp
+      numChoiceYear += 1
+
+      if higherChoice < lifeExp:
+        higherChoice = lifeExp
+        higherCountryChoice = country
+    
+      if lowestChoice > lifeExp:
+        lowestChoice = lifeExp
+        lowestCountryChoice = country
+
+    # Country Choice Code
+    if choiceCountry.lower() == country.lower():
+      if higherChoiceCountry < lifeExp:
+        higherChoiceCountry = lifeExp
+        higherYearChoiceCountry = year
+    
+      if lowestChoiceCountry > lifeExp:
+        lowestChoiceCountry = lifeExp
+        lowestYearChoiceCountry = year
 
 
+print(f"\nThe overall min life expectancy is: {higher} from {higherCountry} in {higherYear}")
 
+print(f"The overall min life expectancy is: {lowest} from {lowestCountry} in {lowestYear}")
 
-# print(f"\nFor the year {user_year}:")
-# print(f"The average life expectancy across all countries was {count_years_amount / count_years:.2f}")
-# print(f"The max life expectancy was in {country_name[index_highest_expectancy_year]} with {life_expectancy[index_highest_expectancy_year]}")
-# print(f"The min life expectancy was in {country_name[index_lowest_expectancy_year]} with {life_expectancy[index_lowest_expectancy_year]}")
+print(f"\nFor the year {choiceYear}:")
+print(f"The average life expectancy across all countries was {average / numChoiceYear:.2f}")
+print(f"\nThe max life expectancy was in {higherCountryChoice} with {higherChoice}")
+print(f"The min life expectancy was in {lowestCountryChoice} with {lowestChoice}")
 
-
-
+print(f"\nFor the Country {choiceCountry}: ")
+print(f"\nThe max life expectancy was in the year {higherYearChoiceCountry} with {higherChoiceCountry}")
+print(f"The min life expectancy was in the year {lowestYearChoiceCountry} with {lowestChoiceCountry}")
 
 
 
